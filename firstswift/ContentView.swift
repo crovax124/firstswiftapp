0694596e8263e7bdb42e70e3b9bb9ed81473f02f
crovax124 <eric.isensee11@gmail.com>
2022-07-14 10:00:40 +0200

    Initial Commit

---
+++ firstswift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  firstswift
+//
+//  Created by Eric Isensee on 14.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstswift/firstswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstswiftApp.swift
+//  firstswift
+//
+//  Created by Eric Isensee on 14.07.22.
+//
+
+import SwiftUI
+
+@main
+struct firstswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
